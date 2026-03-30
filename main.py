b25fa9029a5ee337f04c3a09f49bd56fb1e39cb8
kauamdsouza <kauams2709@gmail.com>
2026-03-30 10:41:42 -0300

    Transformação em web site

--- main.py
+++ main.py
@@ -1,29 +1,57 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 import random
 import requests
 
-#Sorteando um pokémon
-id_pokemon = random.randint(1, 151)
-url = f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}"
-response = requests.get(url).json()
-pokemon_cpu = response["name"].capitalize()
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
+def get_random_pokemon():
+    id_pokemon = random.randint(1, 151)
+    url = f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}"
+    dados = requests.get(url).json()
+    
+    return {
+        "nome" : dados["name"], # Aqui pega o nome (ex: pikachu)
+        "imagem" : dados["sprites"]["front_default"] # Aqui pega a URL da foto
+    }
+
+@app.get("/", response_class= HTMLResponse)
+async def home(request: Request):
+    pokemon = get_random_pokemon()
+    return templates.TemplateResponse(
+    request=request, 
+    name="index.html", 
+    context={"pokemon": pokemon}
+)
+
+@app.post("/jogar", response_class=HTMLResponse)
+async def jogar(request: Request, player_choice: str = Form(...), poke_nome: str = Form(...), poke_img: str = Form(...)):
 #Jogo
-opcoes = ["Pedra", "Papel", "Tesoura"]
-cpu_choice = random.choice(opcoes)
+    opcoes = ["Pedra", "Papel", "Tesoura"]
+    cpu_choice = random.choice(opcoes)
 
-print(f"Um {pokemon_cpu} selvagem apareceu!")
-escolha = input("Escolha pedra, papel ou tesoura: ").title()
-player = escolha
 
-#Lógica de vitória
-print(f"{pokemon_cpu} escolheu: {cpu_choice}")
+# Lógica de vitória
+    if player_choice == cpu_choice:
+        msg = "Empate!"
+    elif (player_choice == "Pedra" and cpu_choice =="Tesoura") or \
+        (player_choice == "Papel" and cpu_choice =="Pedra") or \
+        (player_choice == "Tesoura" and cpu_choice == "Papel"):
+        msg = f"Incrível! Você venceu o {poke_nome}!"
+    else:
+        msg = f"O {poke_nome} massacrou você!"
 
-if player == cpu_choice:
-    print("Empate!")
-elif (player == "Pedra" and cpu_choice =="Tesoura") or \
-     (player == "Papel" and cpu_choice =="Pedra") or \
-     (player == "Tesoura" and cpu_choice == "Papel"):
-    print(f"Incrivel você venceu o {pokemon_cpu}!")
-else:
-    ("Pokemon massacrou sua familia")
+    # No return, envie a variável 'msg'
+    return templates.TemplateResponse(
+    request=request, 
+    name="index.html", 
+    context={
+        "pokemon": {"nome": poke_nome, "imagem": poke_img},
+        "resultado": msg,
+        "escolha_player": player_choice,
+        "cpu_choice": cpu_choice
+    }
+)
     
